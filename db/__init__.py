c6b3bb182007703a08071eb5be7fe07949fa78e0
c1c4 <felipe.arado.pompeu@gmail.com>
2021-09-08 10:59:08 -0300

    Up

--- db/__init__.py
+++ db/__init__.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = f'{DATABASE_URL}_test' if os.environ.get('TESTING') else DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-print('this is the engine', engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
