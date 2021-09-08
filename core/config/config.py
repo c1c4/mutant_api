3d723d835981a1e582eec29931f039194fac4999
c1c4 <felipe.arado.pompeu@gmail.com>
2021-09-08 10:14:12 -0300

    Playing around

--- core/config/config.py
+++ core/config/config.py
@@ -1,14 +1,24 @@
+import os
 from starlette.config import Config
 from starlette.datastructures import Secret
 
-config = Config('.env')
+if os.environ.get('ENV') is None:
 
-SECRET_KEY = config('SECRET_KEY', cast=Secret, default='CHANGEME')
+    config = Config('.env')
 
-POSTGRES_USER = config('POSTGRES_USER', cast=str)
-POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=Secret)
-POSTGRES_SERVER = config('POSTGRES_SERVER', cast=str, default='db')
-POSTGRES_PORT = config('POSTGRES_PORT', cast=str, default='5432')
-POSTGRES_DB = config('POSTGRES_DB', cast=str)
+    SECRET_KEY = config('SECRET_KEY', cast=Secret, default='CHANGEME')
 
-DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
+    POSTGRES_USER = config('POSTGRES_USER', cast=str)
+    POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=Secret)
+    POSTGRES_SERVER = config('POSTGRES_SERVER', cast=str, default='db')
+    POSTGRES_PORT = config('POSTGRES_PORT', cast=str, default='5432')
+    POSTGRES_DB = config('POSTGRES_DB', cast=str)
+
+    DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
+else:
+    print('testing')
+    POSTGRES_USER = os.environ.get('POSTGRES_USER', 'postgres')
+    POSTGRES_SERVER = os.environ.get('POSTGRES_SERVER', 'db')
+    POSTGRES_PORT = os.environ.get('POSTGRES_PORT', '5432')
+    POSTGRES_DB = os.environ.get('POSTGRES_DB', 'db')
+    DATABASE_URL = f'postgresql+psycopg2://{POSTGRES_USER}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
